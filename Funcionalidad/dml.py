c729d6faace7fcf72aae2c54fa8f46c9b3e3fc98
DouglasSochC <3008474750101@ingenieria.usac.edu.gt>
2023-12-07 19:48:38 -0600

    [HOTFIX] Se ha agregado las variables de entorno para el DML

--- Funcionalidad/dml.py
+++ Funcionalidad/dml.py
@@ -1,8 +1,13 @@
 import os
 import xml.etree.ElementTree as ET
 from .util import validar_tipo_dato
+from dotenv import load_dotenv
 
-__CARPETA_PARA_BASES_DE_DATOS='databases/'
+load_dotenv()
+__CARPETA_PARA_BASES_DE_DATOS = str(os.environ.get("CARPETA_PARA_BASES_DE_DATOS"))
+__CARPETA_PARA_TABLAS = str(os.environ.get("CARPETA_PARA_TABLAS"))
+__CARPETA_PARA_FUNCIONES = str(os.environ.get("CARPETA_PARA_FUNCIONES"))
+__CARPETA_PARA_PROCEDIMIENTOS = str(os.environ.get("CARPETA_PARA_PROCEDIMIENTOS"))
 
 def __validar_estructura_tupla(path_xml: str, tupla: list) -> str | dict:
 
@@ -46,6 +51,79 @@ def __validar_estructura_tupla(path_xml: str, tupla: list) -> str | dict:
 
     return tupla_respuesta
 
+def __validar_atributos(path_xml: str, atributos: list) -> str | dict:
+
+    if len(atributos) <= 0:
+        return "Debe de seleccionar por lo menos una columna"
+
+    # Se crea una variable que devolvera un diccionario de los atributos
+    atributos_respuesta = []
+
+    # Se obtiene la raiz del XML
+    tree = ET.parse(path_xml)
+    root = tree.getroot()
+
+    # Buscar la definicion de los campos de la tabla en la estructura
+    campos = root.find(".//estructura")
+
+    # Se valida cada dato de la atributos segun la estructura de la tabla
+    for campo in campos.findall('./campo'):
+
+        campo_nombre = campo.attrib['name']
+
+        # Se verifica que venga el campo
+        if campo_nombre in atributos:
+            atributos_respuesta.append(campo_nombre)
+            atributos.remove(campo_nombre)
+
+    # Debido a que puede ir parametros desconocidos en la atributos parametro, se valida que no vengan demas
+    if len(atributos) > 0:
+        campos_invalidos = ", ".join(list(atributos))
+        return "Las siguientes columnas son invalidas: " + campos_invalidos
+
+    return atributos_respuesta
+
+def __validar_condicion(path_xml: str, condiciones: list) -> str | dict:
+    # Se crea una variable que devolvera un diccionario del registro a insertar
+    tupla_respuesta = {}
+
+    # Se obtiene la raiz del XML
+    tree = ET.parse(path_xml)
+    root = tree.getroot()
+
+    # Buscar la definicion de los campos de la tabla en la estructura
+    campos = root.find(".//estructura")
+
+    # Se valida cada dato de la condiciones segun la estructura de la tabla
+    for campo in campos.findall('./campo'):
+
+        campo_nombre = campo.attrib['name']
+
+        # Se verifica que venga el campo
+        if campo_nombre not in condiciones:
+
+            # Debido a que el campo no viene, se debe validar si puede ser NULL o no
+            if campo.attrib['nullable'] == 'false':
+                return "El campo '" + campo_nombre + "' no puede de ser NULL"
+        else:
+
+            # Debido a que el campo viene, hay que validar su restriccion segun el tipo de dato
+            longitud = campo.attrib['length'] if 'length' in campo.attrib else None
+            val_campo = validar_tipo_dato(campo_nombre, condiciones[campo_nombre], campo.attrib['type'], longitud)
+            if val_campo is not None:
+                return val_campo
+
+            # Se almacena el dato en la condiciones de respuesta y se elimina la llave para la condiciones parametro de este metodo
+            tupla_respuesta[campo_nombre] = condiciones[campo_nombre]
+            del condiciones[campo_nombre]
+
+    # Debido a que puede ir parametros desconocidos en la condiciones parametro, se valida que no vengan demas
+    if len(condiciones) > 0:
+        campos_invalidos = ", ".join(list(condiciones.keys()))
+        return "Las siguientes columnas son invalidas: " + campos_invalidos
+
+    return tupla_respuesta
+
 def insertar_registro_tabla(nombre_bd:str, nombre_tabla: str, tupla: list) -> str:
     '''
     Realiza un registro de una tupla a una tabla
@@ -63,7 +141,7 @@ def insertar_registro_tabla(nombre_bd:str, nombre_tabla: str, tupla: list) -> st
     elif not os.path.exists(__CARPETA_PARA_BASES_DE_DATOS + nombre_bd): # Se valida que exista la base de datos
         return "No existe la base de datos seleccionada"
 
-    path_tabla = __CARPETA_PARA_BASES_DE_DATOS + nombre_bd + "/" + nombre_tabla + ".xml"
+    path_tabla = __CARPETA_PARA_BASES_DE_DATOS + nombre_bd + __CARPETA_PARA_TABLAS + nombre_tabla + ".xml"
     respuesta = __validar_estructura_tupla(path_tabla, tupla)
 
     # Este es el caso que la estructura de la tupla es valida
@@ -92,3 +170,40 @@ def insertar_registro_tabla(nombre_bd:str, nombre_tabla: str, tupla: list) -> st
     # Este es el caso que la estructura de la tupla es valida
     else:
         return respuesta
+
+def seleccionar_registro_tabla(nombre_bd:str, nombre_tabla: str, atributos: list, condiciones: list):
+
+    datos = []
+    path_tabla = __CARPETA_PARA_BASES_DE_DATOS + nombre_bd + __CARPETA_PARA_TABLAS + nombre_tabla + ".xml"
+
+    # Se validan los atributos
+    val_atributos = __validar_atributos(path_tabla, atributos)
+    # val_condicion = __validar_condicion(path_tabla, condiciones)
+
+    # Este es el caso que los atributos son validos para la tabla seleccionada
+    if isinstance(val_atributos, str):
+        return val_atributos
+    # elif isinstance(val_condicion, str):
+    #     return "hola"
+    else:
+        # Se obtiene la raiz del XML
+        tree = ET.parse(path_tabla)
+        root = tree.getroot()
+
+        # Se obtiene la definicion de los registros
+        registros = root.find(".//registros")
+
+        # Se obtiene cada fila
+        filas = registros.findall(".//fila")
+        for fila in filas:
+
+            # Accede a los elementos dentro de la fila
+            elementos_fila = fila.findall(".//*")
+
+            # Se almacenan en una variable para que esta pueda aplicarsele las condiciones
+            resultado_fila = {}
+            for elemento in elementos_fila:
+                resultado_fila[elemento.tag] = elemento.text
+            datos.append(resultado_fila)
+
+        return datos
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from Funcionalidad.ddl import crear_base_de_datos, crear_tabla, eliminar_base_de_datos
-from Funcionalidad.dml import insertar_registro_tabla
+from Funcionalidad.dml import insertar_registro_tabla, seleccionar_registro_tabla
 
 print()
 # Crea la base de datos
@@ -40,8 +40,8 @@ respuesta = crear_tabla("bd1", "tbl1", [
     {'name':'total', 'type':'decimal', 'nullable':'false'},
     {'name':'fecha', 'type':'date', 'nullable':'false'},
     {'name':'fecha_hora', 'type':'datetime', 'nullable':'false'},
-    {'name':'nombre', 'type':'nchar', 'length': '5', 'nullable':'false'},
-    {'name':'descripcion', 'type':'nvarchar', 'length': '10', 'nullable':'false'},
+    {'name':'nombre', 'type':'nchar', 'length': '20', 'nullable':'false'},
+    {'name':'descripcion', 'type':'nvarchar', 'length': '100', 'nullable':'false'},
 ])
 print(respuesta)
 # ERROR | Ya existe la tabla
@@ -95,14 +95,59 @@ print(respuesta)
 respuesta = insertar_registro_tabla("bd1", "tbl1", {'id':'100', 'es_admin':'1', 'total':'125.5', 'fecha':'01-01-2024', 'fecha_hora':'01-01-2024 00:00:00', 'nombre':''})
 print(respuesta)
 # ERROR NCHAR | 'nombre' no debe sobrepasarse la cantidad de caracteres definidos
-respuesta = insertar_registro_tabla("bd1", "tbl1", {'id':'100', 'es_admin':'1', 'total':'125.5', 'fecha':'01-01-2024', 'fecha_hora':'01-01-2024 00:00:00', 'nombre':'abc123'})
+respuesta = insertar_registro_tabla("bd1", "tbl1", {'id':'100', 'es_admin':'1', 'total':'125.5', 'fecha':'01-01-2024', 'fecha_hora':'01-01-2024 00:00:00', 'nombre':'abcdefghijklmnopqrstuvwxyz'})
 print(respuesta)
 # ERROR NVARCHAR | 'descripcion' debe contener por lo menos un caracter
-respuesta = insertar_registro_tabla("bd1", "tbl1", {'id':'100', 'es_admin':'1', 'total':'125.5', 'fecha':'01-01-2024', 'fecha_hora':'01-01-2024 00:00:00', 'nombre':'abcde', 'descripcion':''})
+respuesta = insertar_registro_tabla("bd1", "tbl1", {'id':'100', 'es_admin':'1', 'total':'125.5', 'fecha':'01-01-2024', 'fecha_hora':'01-01-2024 00:00:00', 'nombre':'abcdefghijklmnopqrs', 'descripcion':''})
 print(respuesta)
 # ERROR NVARCHAR | 'descripcion' no debe sobrepasarse la cantidad de caracteres definidos
-respuesta = insertar_registro_tabla("bd1", "tbl1", {'id':'100', 'es_admin':'1', 'total':'125.5', 'fecha':'01-01-2024', 'fecha_hora':'01-01-2024 00:00:00', 'nombre':'abcde', 'descripcion':'123456789ab'})
+respuesta = insertar_registro_tabla("bd1", "tbl1", {'id':'100', 'es_admin':'1', 'total':'125.5', 'fecha':'01-01-2024', 'fecha_hora':'01-01-2024 00:00:00', 'nombre':'abcdefghijklmnopqrs', 'descripcion':'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'})
 print(respuesta)
 # Registro realizado correctamente
-respuesta = insertar_registro_tabla("bd1", "tbl1", {'id':'100', 'es_admin':'1', 'total':'125.5', 'fecha':'01-01-2024', 'fecha_hora':'01-01-2024 00:00:00', 'nombre':'abcde', 'descripcion':'douglas'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '100', 'es_admin': '1', 'total': '125.5', 'fecha': '02-01-2024', 'fecha_hora': '02-01-2024 03:00:00', 'nombre': 'nombre0', 'descripcion': 'descripcion0'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '101', 'es_admin': '0', 'total': '75.2', 'fecha': '03-01-2024', 'fecha_hora': '02-01-2024 06:00:00', 'nombre': 'nombre1', 'descripcion': 'descripcion1'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '102', 'es_admin': '1', 'total': '42.0', 'fecha': '04-01-2024', 'fecha_hora': '02-01-2024 09:00:00', 'nombre': 'nombre2', 'descripcion': 'descripcion2'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '103', 'es_admin': '0', 'total': '150.3', 'fecha': '05-01-2024', 'fecha_hora': '02-01-2024 12:00:00', 'nombre': 'nombre3', 'descripcion': 'descripcion3'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '104', 'es_admin': '1', 'total': '80.1', 'fecha': '06-01-2024', 'fecha_hora': '02-01-2024 15:00:00', 'nombre': 'nombre4', 'descripcion': 'descripcion4'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '105', 'es_admin': '0', 'total': '170.5', 'fecha': '07-01-2024', 'fecha_hora': '02-01-2024 18:00:00', 'nombre': 'nombre5', 'descripcion': 'descripcion5'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '106', 'es_admin': '1', 'total': '95.2', 'fecha': '08-01-2024', 'fecha_hora': '02-01-2024 21:00:00', 'nombre': 'nombre6', 'descripcion': 'descripcion6'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '107', 'es_admin': '0', 'total': '120.8', 'fecha': '09-01-2024', 'fecha_hora': '03-01-2024 00:00:00', 'nombre': 'nombre7', 'descripcion': 'descripcion7'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '108', 'es_admin': '1', 'total': '60.3', 'fecha': '10-01-2024', 'fecha_hora': '03-01-2024 03:00:00', 'nombre': 'nombre8', 'descripcion': 'descripcion8'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '109', 'es_admin': '0', 'total': '110.0', 'fecha': '11-01-2024', 'fecha_hora': '03-01-2024 06:00:00', 'nombre': 'nombre9', 'descripcion': 'descripcion9'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '110', 'es_admin': '1', 'total': '85.7', 'fecha': '12-01-2024', 'fecha_hora': '03-01-2024 09:00:00', 'nombre': 'nombre10', 'descripcion': 'descripcion10'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '111', 'es_admin': '0', 'total': '130.4', 'fecha': '13-01-2024', 'fecha_hora': '03-01-2024 12:00:00', 'nombre': 'nombre11', 'descripcion': 'descripcion11'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '112', 'es_admin': '1', 'total': '75.0', 'fecha': '14-01-2024', 'fecha_hora': '03-01-2024 15:00:00', 'nombre': 'nombre12', 'descripcion': 'descripcion12'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '113', 'es_admin': '0', 'total': '100.2', 'fecha': '15-01-2024', 'fecha_hora': '03-01-2024 18:00:00', 'nombre': 'nombre13', 'descripcion': 'descripcion13'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '114', 'es_admin': '1', 'total': '55.9', 'fecha': '16-01-2024', 'fecha_hora': '03-01-2024 21:00:00', 'nombre': 'nombre14', 'descripcion': 'descripcion14'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '115', 'es_admin': '0', 'total': '105.6', 'fecha': '17-01-2024', 'fecha_hora': '04-01-2024 00:00:00', 'nombre': 'nombre15', 'descripcion': 'descripcion15'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '116', 'es_admin': '1', 'total': '80.3', 'fecha': '18-01-2024', 'fecha_hora': '04-01-2024 03:00:00', 'nombre': 'nombre16', 'descripcion': 'descripcion16'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '117', 'es_admin': '0', 'total': '120.0', 'fecha': '19-01-2024', 'fecha_hora': '04-01-2024 06:00:00', 'nombre': 'nombre17', 'descripcion': 'descripcion17'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '118', 'es_admin': '1', 'total': '95.7', 'fecha': '20-01-2024', 'fecha_hora': '04-01-2024 09:00:00', 'nombre': 'nombre18', 'descripcion': 'descripcion18'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '119', 'es_admin': '0', 'total': '140.4', 'fecha': '21-01-2024', 'fecha_hora': '04-01-2024 12:00:00', 'nombre': 'nombre19', 'descripcion': 'descripcion19'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '120', 'es_admin': '1', 'total': '65.2', 'fecha': '22-01-2024', 'fecha_hora': '04-01-2024 15:00:00', 'nombre': 'nombre20', 'descripcion': 'descripcion20'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '121', 'es_admin': '0', 'total': '110.9', 'fecha': '23-01-2024', 'fecha_hora': '04-01-2024 18:00:00', 'nombre': 'nombre21', 'descripcion': 'descripcion21'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '122', 'es_admin': '1', 'total': '75.6', 'fecha': '24-01-2024', 'fecha_hora': '04-01-2024 21:00:00', 'nombre': 'nombre22', 'descripcion': 'descripcion22'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '123', 'es_admin': '0', 'total': '105.3', 'fecha': '25-01-2024', 'fecha_hora': '05-01-2024 00:00:00', 'nombre': 'nombre23', 'descripcion': 'descripcion23'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '124', 'es_admin': '1', 'total': '85.1', 'fecha': '26-01-2024', 'fecha_hora': '05-01-2024 03:00:00', 'nombre': 'nombre24', 'descripcion': 'descripcion24'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '125', 'es_admin': '0', 'total': '125.8', 'fecha': '27-01-2024', 'fecha_hora': '05-01-2024 06:00:00', 'nombre': 'nombre25', 'descripcion': 'descripcion25'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '126', 'es_admin': '1', 'total': '100.5', 'fecha': '28-01-2024', 'fecha_hora': '05-01-2024 09:00:00', 'nombre': 'nombre26', 'descripcion': 'descripcion26'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '127', 'es_admin': '0', 'total': '145.2', 'fecha': '29-01-2024', 'fecha_hora': '05-01-2024 12:00:00', 'nombre': 'nombre27', 'descripcion': 'descripcion27'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '128', 'es_admin': '1', 'total': '70.0', 'fecha': '30-01-2024', 'fecha_hora': '05-01-2024 15:00:00', 'nombre': 'nombre28', 'descripcion': 'descripcion28'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '129', 'es_admin': '0', 'total': '115.7', 'fecha': '31-01-2024', 'fecha_hora': '05-01-2024 18:00:00', 'nombre': 'nombre29', 'descripcion': 'descripcion29'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '130', 'es_admin': '1', 'total': '90.4', 'fecha': '01-02-2024', 'fecha_hora': '05-01-2024 21:00:00', 'nombre': 'nombre30', 'descripcion': 'descripcion30'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '131', 'es_admin': '0', 'total': '135.1', 'fecha': '02-02-2024', 'fecha_hora': '06-01-2024 00:00:00', 'nombre': 'nombre31', 'descripcion': 'descripcion31'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '132', 'es_admin': '1', 'total': '60.8', 'fecha': '03-02-2024', 'fecha_hora': '06-01-2024 03:00:00', 'nombre': 'nombre32', 'descripcion': 'descripcion32'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '133', 'es_admin': '0', 'total': '110.5', 'fecha': '04-02-2024', 'fecha_hora': '06-01-2024 06:00:00', 'nombre': 'nombre33', 'descripcion': 'descripcion33'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '134', 'es_admin': '1', 'total': '85.2', 'fecha': '05-02-2024', 'fecha_hora': '06-01-2024 09:00:00', 'nombre': 'nombre34', 'descripcion': 'descripcion34'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '135', 'es_admin': '0', 'total': '130.9', 'fecha': '06-02-2024', 'fecha_hora': '06-01-2024 12:00:00', 'nombre': 'nombre35', 'descripcion': 'descripcion35'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '136', 'es_admin': '1', 'total': '75.6', 'fecha': '07-02-2024', 'fecha_hora': '06-01-2024 15:00:00', 'nombre': 'nombre36', 'descripcion': 'descripcion36'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '137', 'es_admin': '0', 'total': '105.3', 'fecha': '08-02-2024', 'fecha_hora': '06-01-2024 18:00:00', 'nombre': 'nombre37', 'descripcion': 'descripcion37'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '138', 'es_admin': '1', 'total': '80.0', 'fecha': '09-02-2024', 'fecha_hora': '06-01-2024 21:00:00', 'nombre': 'nombre38', 'descripcion': 'descripcion38'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '139', 'es_admin': '0', 'total': '120.7', 'fecha': '10-02-2024', 'fecha_hora': '07-01-2024 00:00:00', 'nombre': 'nombre39', 'descripcion': 'descripcion39'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '140', 'es_admin': '1', 'total': '95.4', 'fecha': '11-02-2024', 'fecha_hora': '07-01-2024 03:00:00', 'nombre': 'nombre40', 'descripcion': 'descripcion40'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '141', 'es_admin': '0', 'total': '135.1', 'fecha': '12-02-2024', 'fecha_hora': '07-01-2024 06:00:00', 'nombre': 'nombre41', 'descripcion': 'descripcion41'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '142', 'es_admin': '1', 'total': '110.8', 'fecha': '13-02-2024', 'fecha_hora': '07-01-2024 09:00:00', 'nombre': 'nombre42', 'descripcion': 'descripcion42'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '143', 'es_admin': '0', 'total': '155.5', 'fecha': '14-02-2024', 'fecha_hora': '07-01-2024 12:00:00', 'nombre': 'nombre43', 'descripcion': 'descripcion43'})
+insertar_registro_tabla("bd1", "tbl1", {'id': '144', 'es_admin': '1', 'total': '80.2', 'fecha': '15-02-2024', 'fecha_hora': '07-01-2024 15:00:00', 'nombre': 'nombre44', 'descripcion': 'descripcion44'})
+respuesta = insertar_registro_tabla("bd1", "tbl1", {'id': '145', 'es_admin': '0', 'total': '125.9', 'fecha': '16-02-2024', 'fecha_hora': '07-01-2024 18:00:00', 'nombre': 'nombre45', 'descripcion': 'descripcion45'})
 print(respuesta)
