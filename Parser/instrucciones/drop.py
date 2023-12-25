cc9d4515f5a5f587e52d8ac33325a6b048ffe52a
Alex4191-usac <3444601420101@ingenieria.usac.edu.gt>
2023-12-24 20:25:38 -0600

    se reestrablecieron los valores de ejecucion y el main de prueba

--- Parser/instrucciones/drop.py
+++ Parser/instrucciones/drop.py
@@ -11,7 +11,35 @@ class Drop(Instruccion):
         self.identificador = identificador
 
     def Ejecutar(self, base_datos, entorno):
-        pass
+
+        # Se verifica que no se este utilizando este 'DROP' dentro de la creacion de un procedimiento o funcion
+        construccion = entorno.obtener("construir_procedimiento")
+        construccion = construccion if construccion is not None else entorno.obtener("construir_funcion")
+        if construccion is not None:
+            return RetornoError("No puede utilizar el comando 'DROP' dentro de la creacion de un PROCEDURE o FUNCTION")
+
+        if base_datos.valor == "":
+            return RetornoError("Para ejecutar el comando 'DROP', es necesario seleccionar una base de datos.")
+
+        # Se obtiene el nombre
+        res_identificador = self.identificador.Ejecutar(base_datos, entorno)
+        nombre = res_identificador['identificador']
+
+        dll = DDL()
+        respuesta = None
+        if self.tipo_eliminacion == 'database':
+            respuesta = dll.eliminar_base_de_datos(nombre)
+            if nombre == base_datos.valor:
+                base_datos.valor = ""
+        elif self.tipo_eliminacion == 'table':
+            respuesta = dll.eliminar_tabla(base_datos.valor, nombre)
+        elif self.tipo_eliminacion == 'procedure':
+            respuesta = dll.eliminar_procedimiento(base_datos.valor, nombre)
+        elif self.tipo_eliminacion == 'function':
+            respuesta = dll.eliminar_funcion(base_datos.valor, nombre)
+
+        return RetornoCorrecto(respuesta.valor) if respuesta.success else RetornoError(respuesta.valor)
+
     def GraficarArbol(self, id_padre):
         label_encabezado =  "\"{}\"[label=\"{}\"];\n".format(self.id_nodo, "DROP")
         label_tipo = "\"{}\"[label=\"{}\"];\n".format(self.id_nodo + "C", self.tipo_eliminacion)
--- Parser/instrucciones/use.py
+++ Parser/instrucciones/use.py
@@ -10,7 +10,24 @@ class Use(Instruccion):
         pass
 
     def Ejecutar(self, base_datos, entorno):
-        pass
+
+        # Se verifica que no se este utilizando este 'USE' dentro de la creacion de un procedimiento o funcion
+        construccion = entorno.obtener("construir_procedimiento")
+        construccion = construccion if construccion is not None else entorno.obtener("construir_funcion")
+        if construccion is not None:
+            return RetornoError("No puede utilizar el comando 'USE' dentro de la creacion de un PROCEDURE o FUNCTION")
+
+        nombre = self.texto
+
+        administracion = Administracion()
+
+        res_admin = administracion.verificar_existencia_bd(nombre)
+
+        if res_admin.success:
+            base_datos.valor = nombre
+            return RetornoCorrecto(res_admin.valor)
+        else:
+            return RetornoError(res_admin.valor)
 
     def GraficarArbol(self, id_padre):
         label_encabezado =  "\"{}\"[label=\"{}\"];\n".format(self.id_nodo, "USE")
