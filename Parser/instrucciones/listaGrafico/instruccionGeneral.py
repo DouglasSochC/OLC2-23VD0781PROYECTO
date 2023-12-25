389c662630e01d654aeefb37ba54816684e9bda3
DouglasSochC <3008474750101@ingenieria.usac.edu.gt>
2023-12-25 14:01:54 -0600

    [UPDATE] Se ha implementado el metodo Ejecutar de la instrucción 'instruccionGeneral'

--- Parser/instrucciones/listaGrafico/instruccionGeneral.py
+++ Parser/instrucciones/listaGrafico/instruccionGeneral.py
@@ -2,13 +2,14 @@ from ...abstract.instrucciones import Instruccion
 
 class InstruccionGeneral(Instruccion):
 
-    def __init__(self, id_nodo: int, instruccion: any):
+    def __init__(self, id_nodo: str, instruccion: any):
         self.id_nodo = id_nodo
         self.instruccion = instruccion
-       
 
     def Ejecutar(self, base_datos, entorno):
-        pass
+        res_ejecutar_instruccion = self.instruccion.Ejecutar(base_datos, entorno)
+        return res_ejecutar_instruccion
+
     def GraficarArbol(self, id_padre):
         label_inicio = "\"{}\"[label=\"{}\"];\n".format("0INICIO", "INICIO")
         label_encabezado =  "\"{}\"[label=\"{}\"];\n".format(self.id_nodo, "INSTRUCCION")
